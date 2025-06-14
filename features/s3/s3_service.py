59f8752db55dade384de14e98e10f118e32438d3
ErnestStudentBTU <ernest.georkiani.1@btu.edu.ge>
2025-06-14 16:41:35 +0000

    continue cli

--- features/s3/s3_service.py
+++ features/s3/s3_service.py
@@ -1,5 +1,6 @@
 from types_boto3_s3 import S3Client
-
+import os
+from pathlib import Path
 
 class S3Service:
     __s3_client: S3Client
@@ -12,9 +13,20 @@ class S3Service:
     def upload_file(self, source: str, dest: str) -> None:
         self.__s3_client.upload_file(source, self.__bucket_name, dest)
 
-    def download_file(self, source: str, dest: str) -> None:
-        self.__s3_client.download_file(self.__bucket_name, source, dest)
+    def __get_file_list_recursive(self, source_folder: str) -> list[str]:
+        full_path_to_source_folder = os.path.realpath(source_folder)
+        files = []
 
-    def download_file_bytes(self, source: str) -> bytes:
-        response = self.__s3_client.get_object(Bucket=self.__bucket_name, Key=source)
-        return response["Body"].read()
+        for (dirpath, dirnames, filenames) in os.walk(source_folder):
+            for filename in filenames:
+                full_path = os.path.realpath(os.path.join(dirpath, filename))
+                relative_path = Path(full_path).relative_to(full_path_to_source_folder)
+                files.append(str(relative_path))
+        return files
+
+    def upload_folder(self, source_folder: str) -> None:
+        source_folder_path = Path(source_folder)
+        files = self.__get_file_list_recursive(source_folder)
+        for files in files:
+            self.upload_file(os.path.join(source_folder, file), file)
+        
